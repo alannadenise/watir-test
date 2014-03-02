d6edee95ccde2589425286e61f59cae832fd9ed1
Alanna Edwards <lanadenise@gmail.com>
2014-03-02 13:43:28 +0000

    Test to create a message in gmail.

--- test.rb
+++ test.rb
@@ -1,9 +1,24 @@
 require 'watir-webdriver'
+require 'highline/import'
+
+puts 'please enter your username:'
+username = gets.chomp
+password = HighLine.ask("please enter your password:") { |q| q.echo = "*" }
+puts 'please enter an email address:'
+emailaddress = gets.chomp
+puts 'please enter your subject:'
+subject = gets.chomp
 
 b = Watir::Browser.new 
-b.goto 'http://bit.ly/watir-webdriver-demo'
-b.text_field(:id => 'entry_0').set 'your name'
-b.select_list(:id => 'entry_1').select 'Ruby'
-b.select_list(:id => 'entry_1').selected? 'Ruby'
-b.button(:name => 'submit').click
-b.text.include? 'Thank you'
+
+b.goto 'http://gmail.com'
+b.text_field(:id => 'Email').set username
+b.text_field(:id => 'Passwd').set password
+b.button(:name => 'signIn').click
+b.div(:class => 'T-I J-J5-Ji T-I-KE L3').wait_until_present
+b.div(:class => 'T-I J-J5-Ji T-I-KE L3').click
+b.textarea(:name => 'to').wait_until_present
+b.textarea(:name => 'to').set emailaddress
+b.text_field(:name => 'subjectbox').set subject
+
+#gmail are clever (well done gmail!) so sending the email can't be done like this
